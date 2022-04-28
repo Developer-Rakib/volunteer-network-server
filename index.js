03a5b17dfbe6f4bfcb1a48d5a6a44ae05e725afc
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-04-28 19:41:53 +0600

    delete api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware 
@@ -17,11 +17,47 @@ const run = async () => {
     try {
         await client.connect();
         const VolunteerEventsCollection = client.db("volunteer").collection("events");
+        const SavedEventsCollection = client.db("volunteer").collection("SaveEvents");
+
+        // get all 
         app.get("/events", async (req, res) => {
             const query = {};
             const cursor = VolunteerEventsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
+        })
+
+        // get by id 
+        app.get("/events/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await VolunteerEventsCollection.findOne(query);
+            res.send(result)
+        })
+
+        // get all Saved data
+        app.get("/SavedEvents", async (req, res) => {
+            const query = {};
+            const cursor = SavedEventsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        // post Saved data
+        app.post("/SavedEvents", async (req, res) => {
+            const event = req.body;
+            if (!event.title || !event.img) {
+                return res.send({ success: false, message: "data not found" })
+            }
+            const result = await SavedEventsCollection.insertOne(event);
+            res.send({ success: true, message: `Successfully Saved ${event.title}`, result })
+        })
+
+        // delete seved data
+        app.delete("/SavedEvents/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await SavedEventsCollection.deleteOne(query);
+            res.send({ success: true, message: "Cancel Successfull", result })
         })
 
     } finally {
