cd9d1ff34d74752c34c93c76659745b8c9ab2015
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-04-28 15:11:59 +0600

    get method ceated

--- index.js
+++ index.js
@@ -2,20 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 // middleware 
 app.use(express.json())
 app.use(cors())
 
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.nbflg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nbflg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+const run = async () => {
+    try {
+        await client.connect();
+        const VolunteerEventsCollection = client.db("volunteer").collection("events");
+        app.get("/events", async (req, res) => {
+            const query = {};
+            const cursor = VolunteerEventsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir)
+
 
 
 
